26e9f2619da1faed43aa611602894db85025800e
aniket <aniket.chanana@gmail.com>
2024-05-12 16:23:28 +0530

    Added static caching for some files

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,13 @@
+var deferredPrompt;
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
+    // .register('/sw.js', {scope: ''}) by using scope property in the register we can tell sw which folder to target
     .register('/sw.js')
     .then(() => console.log('Service worker registered'));
 }
+
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
---
+++ public/sw.js
@@ -0,0 +1,10 @@
+const version = 'v1.0';
+self.addEventListener('install', (event) => {});
+
+self.addEventListener('activate', (event) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
